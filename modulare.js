282242836845f07703f6789a4ddacd60cdd547ba
modulare <79155519+modulare@users.noreply.github.com>
2021-02-25 23:11:27 +0100

    con errori PRIMA versione con 25 body
    i range degli oggetti sono sbagliati e i body alla fine spariscono dal quadro

--- modulare.js
+++ modulare.js
@@ -6,52 +6,51 @@ let orX;
 let orY;
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(1000, 800);
     let orX = width / 2;
     let orY = height / 2;
     this.sni = [];
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 25; i++) {
         let kind = ceil(random(0, 2))
         let vX = random(-1, 1);
         let vY = random(-1, 1);
-        let rot = 0.8;
-        this.sni.push(new snip(i.toString(), orX, orY, new body(kind), createVector(vX, vY),rot));
+        let rot = random(-0.8, 0.8);
+        this.sni.push(new snip(i.toString(), orX, orY, new body(kind), createVector(vX, vY), rot));
     }
 }
 
 function draw() {
-    //TODO: DEFINIRE IL RANGE DI OGNI BODY
-    //TODO: CONTROLLARE SE ATTRAVERSA IL BORDO NON AGGIUNGERE VELOCITA' E ROTAZIONE
-    //TODO: inherit di body
     background(255, 255, 255, 25);
-    /*
-  if (frameCount == 800) {
-    for (let i = 0; i < 8; i++) {
-      this.sni[i].bod.type = 1;
-    }
-  }
 
-  if (frameCount == 1300) {
-    for (let i = 8; i < 16; i++) {
-      this.sni[i].bod.type = 0;
+    if (frameCount == 800) {
+        for (let i = 0; i < 8; i++) {
+            this.sni[i].bod.kind = 1;
+            this.sni[i].range_ = this.sni[i].bod.set_range_(1);
+        }
     }
-  }
-  
-   if (frameCount == 2000) {
-    for (let i = 0; i < 25; i++) {
-      this.sni[i].bod.type = 2;
+
+    if (frameCount == 1300) {
+        //marg = 10;
+        for (let i = 8; i < 16; i++) {
+            this.sni[i].bod.kind = 0;
+            this.sni[i].range_ = this.sni[i].bod.set_range_(0);
+
+        }
     }
-  }
-*/
-    //BOOKMARK: QUI SI FISSA IL TIPO DI BODY
-    this.sni[0].bod.type = 1; //                                    SOLO PER TEST
-    //print("------");
+
+    if (frameCount == 2000) {
+        for (let i = 0; i < 25; i++) {
+            this.sni[i].bod.kind = 2;
+            this.sni[i].range_ = this.sni[i].bod.set_range_(2);
+        }
+    }
+
+
     for (const s of this.sni) {
         s.move();
-        s.bod.display(s.x, s.y,s.rot);
+        s.bod.display(s.x, s.y, s.rot);
         s.borders();
-        //print(s.x,s.y);
     }
 
     noFill();
@@ -61,18 +60,15 @@ function draw() {
 
 class snip {
 
-    constructor(name, x, y, bod, vel,vel_rot) {
+    constructor(name, x, y, bod, vel, vel_rot) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.rot = vel_rot;
-        this.vel_rot=vel_rot;
+        this.vel_rot = vel_rot;
         this.bod = bod;
         this.range_ = bod.range_;
-        print(this.range_);
         this.vel = vel;
-        //FIXME: valore iniziale potrebbe anche essere anche true 
-        this.onBorders = false;
     }
 
     move() {
@@ -89,7 +85,7 @@ class snip {
                 this.y = height - marg - this.range_ / 2;
             }
             k = 1;
-            this.bod.display(this.x, this.y + (height - 2 * marg),this.rot);
+            this.bod.display(this.x, this.y + (height - 2 * marg), this.rot);
             this.displayRange(this.x, this.y + (height - 2 * marg));
         }
         if (this.y + this.range_ / 2 > height - marg) {
@@ -97,7 +93,7 @@ class snip {
                 this.y = marg + this.range_ / 2;
             }
             k += 3;
-            this.bod.display(this.x, this.y - (height - 2 * marg),this.rot);
+            this.bod.display(this.x, this.y - (height - 2 * marg), this.rot);
             this.displayRange(this.x, this.y - (height - 2 * marg));
         }
         if (this.x - this.range_ / 2 < marg) {
@@ -105,7 +101,7 @@ class snip {
                 this.x = width - marg - this.range_ / 2;
             }
             k += 50;
-            this.bod.display(this.x + (width - 2 * marg), this.y,this.rot);
+            this.bod.display(this.x + (width - 2 * marg), this.y, this.rot);
             this.displayRange(this.x + (width - 2 * marg), this.y);
         }
         if (this.x + this.range_ / 2 > width - marg) {
@@ -113,25 +109,24 @@ class snip {
                 this.x = marg + this.range_ / 2;
             }
             k += 70;
-            this.bod.display(this.x - (width - 2 * marg), this.y,this.rot);
+            this.bod.display(this.x - (width - 2 * marg), this.y, this.rot);
             this.displayRange(this.x - (width - 2 * marg), this.y);
         }
-        //BOOKMARK: SWITCH K BORDERS 71 73 51 ....
         switch (k) {
             case 71:
-                this.bod.display(this.x - (width - 2 * marg), this.y + (height - 2 * marg),this.rot);
-                    this.displayRange(this.x - (width - 2 * marg), this.y + (height - 2 * marg));
+                this.bod.display(this.x - (width - 2 * marg), this.y + (height - 2 * marg), this.rot);
+                this.displayRange(this.x - (width - 2 * marg), this.y + (height - 2 * marg));
                 break;
             case 73:
-                this.bod.display(this.x - (width - 2 * marg), this.y - (height - 2 * marg),this.rot);
+                this.bod.display(this.x - (width - 2 * marg), this.y - (height - 2 * marg), this.rot);
                 this.displayRange(this.x - (width - 2 * marg), this.y - (height - 2 * marg));
                 break;
             case 51:
-                this.bod.display(this.x + (width - 2 * marg), this.y + (height - 2 * marg),this.rot);
+                this.bod.display(this.x + (width - 2 * marg), this.y + (height - 2 * marg), this.rot);
                 this.displayRange(this.x + (width - 2 * marg), this.y + (height - 2 * marg));
                 break;
             case 53:
-                this.bod.display(this.x + (width - 2 * marg), this.y - (height - 2 * marg),this.rot);
+                this.bod.display(this.x + (width - 2 * marg), this.y - (height - 2 * marg), this.rot);
                 this.displayRange(this.x + (width - 2 * marg), this.y - (height - 2 * marg));
                 break;
             default:
@@ -147,44 +142,39 @@ class snip {
 }
 
 class body {
-    constructor(type) {
-        this.type = type;
+    constructor(kind) {
+        this.kind = kind;
         this.r = random(0, 255);
         this.g = random(0, 255);
         this.b = random(0, 255);
-        this.set_range_(type);
+        this.set_range_(kind);
         this.rot = 0.8;
     }
-    /*getType(type) {
-        this.type = type;
-    }*/
 
-    set_range_(type) {
-        //BOOKMARK: SWITCH set_range_(type) ma non funziona è ininfluente
-        switch (this.type) {
+    set_range_(kind) {
+        switch (kind) {
             case 0:
-                this.range_ = 0;
+                this.range_ = 100;
+                break;
             case 1:
                 this.range_ = 92;
+                break;
             case 2:
-                this.range_ = 200;
+                this.range_ = 141;
+                break;
             default:
             //
         }
     }
-    
-    display(x, y,rot) {
-        //print(this.type);
-        //BOOKMARK: SWITCH DISPLAY PER TIPO
-        switch (this.type) {
+
+    display(x, y, rot) {
+        switch (this.kind) {
             case 0:
                 noStroke();
                 push();
                 translate(x, y);
-                //area di visuale dello snip
                 fill(this.r, this.g, this.b, 100);
-                ellipse(0, 0, 100); // this.range_ !!!!!!!!!!
-                //nucleo snip
+                ellipse(0, 0, 100); 
                 fill(this.b, this.r, this.g, 100);
                 ellipse(0, 0, 16);
                 pop();
@@ -205,12 +195,12 @@ class body {
                 vertex(-47, -17);
                 endShape(CLOSE);
                 pop();
-                //}
                 break;
             case 2:
                 stroke(1);
                 push();
                 translate(x, y);
+                rotate(radians(rot));
                 beginShape();
                 vertex(-12, -2);
                 vertex(13, -7);
@@ -221,8 +211,6 @@ class body {
                 pop();
                 break;
             default:
-                print("bad");
-            //console.log(`bad`);
         }
     }
 }
